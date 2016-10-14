a3b57d6a41965d29098fa23ae3ba1b45ece149bb
Hendy Rodrigues F. Silva <mr.syslogg@gmail.com>
2016-10-14 15:25:31 -0300

    Add modal action in js

--- src/js/hrstyle.js
+++ src/js/hrstyle.js
@@ -1,3 +1,36 @@
 $(function(){
     
+    
+    //- Settings
+    $('body').append('<div class="modal-background"></div>');
+
+    //- Classes: Modal
+    var bgModal = $('.modal-background');
+    var closeModal = $('.modal-close');
+    var openModal = $('[rel="hr:modal"]');
+    
+    //- Functions
+    //Function that calls a modal
+    //Params: Element: Modal element
+    var modal = function(element) {
+        bgModal.fadeIn("fast",function(){
+            element.fadeIn("fast");
+        });
+    }
+    
+    //- Events
+    //Click to open a modal
+    openModal.click(function(){
+        var modalT = $($(this).attr('href'));
+        modal(modalT);
+    });
+    
+    //Click to close a modal
+    closeModal.click(function(){
+        $(this).parent('.modal').fadeOut("fast",function(){
+            bgModal.fadeOut();
+        });
+
+    });
+
 });
